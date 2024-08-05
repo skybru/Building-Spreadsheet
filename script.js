0a15ff1f12e2dc70dc9706181f9aa6511c9120cb
Jojo <giorgio.bruschi.ext@luxottica.com>
2024-08-05 17:40:39 +0200

    added functions for calculate on the spreadsheet

--- script.js
+++ script.js
@@ -17,10 +17,48 @@ window.onload = () => {
         letters.forEach(letter => {
             const input = document.createElement("input");
             input.type = "text";
-            input.id = letter+number;
+            input.id = letter + number;
+            input.ariaLabel = letter + number;
+            input.onchange = update;
+            container.appendChild(input);
         });
     });
 };
 
 const range = (start, end) => Array(end - start + 1).fill(start).map((item, index) => item + index);
 const charRange = (start, end) => range(start.charCodeAt(0), end.charCodeAt(0)).map((code) => String.fromCharCode(code));
+
+const sum = (nums) => nums.reduce((acc, item) => acc + item, 0);
+const isEven = (num) => num % 2 === 0;
+
+const average = (nums) => sum(nums) / nums.length;
+const median = (nums) => {
+    const sorted = nums.slice().sort((a, b) => a - b);
+    const length = sorted.length;
+    //const middle = Math.floor((length / 2) - 1);
+    const middle = length / 2 - 1;
+    return isEven(length) ? average([sorted[middle], sorted[middle + 1]]) : sorted[Math.ceil(middle)];
+};
+
+const spreadsheetFunctions = {
+    sum,
+    average,
+    median
+};
+
+const update = (event) => {
+    const element = event.target;
+    const value = element.value.replace(/\s/g, "");
+    if (!value.includes(element.id) && value.startsWith("=")) {
+
+    }
+};
+
+const evalFormula = (x, cells) => {
+    const idToText = (id) => cells.find((cell) => cell.id === id).value;
+    const rangeRegex = /([A-J])([1-9][0-9]?):([A-J])([1-9][0-9]?)/gi;
+    const rangeFromString = (num1, num2) => range(parseInt(num1), parseInt(num2));
+    const elemValue = (num) => (character) => idToText(character + num);
+
+    const addCharacters = (character1) => (character2) => (num) => charRange(character1, character2).map(() => {});
+};
